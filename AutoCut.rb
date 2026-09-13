7a453d34ae5237eebae7af3002d3a857cf00d72e
mehoweck <mehoweck@gmail.com>
2026-09-14 01:28:41 +0200

    Refactor: compute_optimization takes explicit params, recalculate delegates to it

--- AutoCut.rb
+++ AutoCut.rb
@@ -31,35 +31,27 @@ module AutoCut
     end
 
     aggregated     = Aggregator.aggregate(instances)
-    initial_result = compute_optimization(aggregated)
+    initial_result = compute_optimization(aggregated,
+                       Settings.source_lengths_str, Settings.cut_loss, Settings.solver_name)
 
     recalculate = ->(lengths_str, cut_loss_val, solver_name) {
       Settings.save(lengths_str, cut_loss_val, solver_name)
-      lengths       = Settings.parse_lengths(lengths_str)
-      lengths       = [200.0] if lengths.empty?
-      cross_results = Optimizer.optimize_by_cross(aggregated, lengths, cut_loss_val,
-                        solver: Optimizer.solver_for(solver_name))
-      {
-        cross_results: cross_results,
-        order:         Optimizer.build_order(cross_results),
-        lengths:       lengths,
-        cut_loss:      cut_loss_val
-      }
+      compute_optimization(aggregated, lengths_str, cut_loss_val, solver_name)
     }
 
     DialogManager.show(instances, aggregated, initial_result, scope_label, recalculate: recalculate)
   end
 
-  def self.compute_optimization(aggregated)
-    lengths       = Settings.parse_lengths(Settings.source_lengths_str)
+  def self.compute_optimization(aggregated, lengths_str, cut_loss, solver_name)
+    lengths       = Settings.parse_lengths(lengths_str)
     lengths       = [200.0] if lengths.empty?
-    cross_results = Optimizer.optimize_by_cross(aggregated, lengths, Settings.cut_loss,
-                      solver: Optimizer.solver_for(Settings.solver_name))
+    cross_results = Optimizer.optimize_by_cross(aggregated, lengths, cut_loss,
+                      solver: Optimizer.solver_for(solver_name))
     {
       cross_results: cross_results,
       order:         Optimizer.build_order(cross_results),
       lengths:       lengths,
-      cut_loss:      Settings.cut_loss
+      cut_loss:      cut_loss
     }
   end
 
